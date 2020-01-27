0cb4eb11884d0365f54f29744a88eaa794de408e
root <devanshguptamrt@gmail.com>
2020-01-27 21:52:57 +0530

    test infra deployment included

---
+++ test_infra.tf
@@ -0,0 +1,50 @@
+#Infrastructure to run deploy testing infrastructure
+
+variable "doToken" {}
+
+
+
+provider "digitalocean" {
+  token = var.doToken
+}
+  name = "key"
+
+data "digitalocean_ssh_key" "keys" {
+}
+
+
+#Droplet for LB
+resource "digitalocean_droplet" "lb" {
+  region             = "blr1"
+  image              = "ubuntu-18-04-x64"
+  size               = "s-1vcpu-1gb"
+  name               = "lb"
+  tags               = ["testing", "momo"]
+  monitoring         = "true"
+  private_networking = "true"
+  ssh_keys           = [data.digitalocean_ssh_key.keys.id]
+}
+
+#Droplets for Backend
+
+resource "digitalocean_droplet" "b1" {
+  region             = "blr1"
+  image              = "ubuntu-18-04-x64"
+  size               = "s-1vcpu-1gb"
+  name               = "lb"
+  tags               = ["testing", "momo"]
+  monitoring         = "true"
+  private_networking = "true"
+  ssh_keys           = [data.digitalocean_ssh_key.keys.id]
+}
+
+resource "digitalocean_droplet" "b2" {
+  region             = "blr1"
+  image              = "ubuntu-18-04-x64"
+  size               = "s-1vcpu-1gb"
+  name               = "lb"
+  tags               = ["testing", "momo"]
+  monitoring         = "true"
+  private_networking = "true"
+  ssh_keys           = [data.digitalocean_ssh_key.keys.id]
+}
